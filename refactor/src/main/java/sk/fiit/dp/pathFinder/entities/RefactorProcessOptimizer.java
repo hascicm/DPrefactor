4699be66a3381f16f0edf181c2dbbd6520939579
hasci <hasci@Hascic>
2017-12-04 20:41:24 +0100

    gitIngore Fix

---
+++ refactor/src/main/java/sk/fiit/dp/pathFinder/entities/RefactorProcessOptimizer.java
@@ -0,0 +1,55 @@
+package sk.fiit.dp.pathFinder.entities;
+
+import java.util.List;
+
+import sk.fiit.dp.pathFinder.dataprovider.DataProvider;
+import sk.fiit.dp.pathFinder.dataprovider.DatabaseDataProvider;
+import sk.fiit.dp.pathFinder.entities.stateSpace.Relation;
+import sk.fiit.dp.pathFinder.usecases.DefaultPathSearchStrategy;
+import sk.fiit.dp.pathFinder.usecases.MinMaxProbabilityCalculationStrategy;
+import sk.fiit.dp.pathFinder.usecases.PathSearchStrategy;
+import sk.fiit.dp.pathFinder.usecases.RelationCreator;
+
+public class RefactorProcessOptimizer {
+	
+	private DataProvider dataProvider;
+	private PathSearchStrategy pathSearchStrategy;
+	private List<Relation> optimalPath; 	
+
+	public RefactorProcessOptimizer(){
+		init();	
+	}
+
+	private void init() {
+		//this.dataProvider = new DatabaseDataProvider();
+		this.dataProvider = new DatabaseDataProvider(); 
+		this.pathSearchStrategy = new DefaultPathSearchStrategy(new RelationCreator(this.dataProvider.getSmellTypes(), this.dataProvider.getRepairs()));
+		//this.pathSearchStrategy = new BeePathSearchStrategy(new RelationCreator(this.dataProvider.getSmellTypes(), this.dataProvider.getRepairs()));
+	
+		//Probability Calculation Strategy
+		this.pathSearchStrategy.setProbabolityCalculationStrategy(new MinMaxProbabilityCalculationStrategy());
+	}
+
+	public void findRefactoringPath(){
+		//MultiAgent ma = new MultiAgent();
+		//ma.findPath(this.dataProvider.getRootState(), this.pathSearchStrategy);
+		Long startTime = System.currentTimeMillis();
+		optimalPath = this.pathSearchStrategy.findPath(this.dataProvider.getRootState(), 0);
+		System.out.println("");
+		System.out.println("Time: ");
+		System.out.println(((startTime - System.currentTimeMillis())/1000.0));
+	}
+	
+	public void setPathSearchStrategy(PathSearchStrategy pathSearchStrategy) {
+		this.pathSearchStrategy = pathSearchStrategy;
+	}
+	
+	public List<Relation> getOptimalPath() {
+		return optimalPath;
+	}
+	public DataProvider getDataProvider(){
+		return this.dataProvider;
+	}
+
+	
+}
